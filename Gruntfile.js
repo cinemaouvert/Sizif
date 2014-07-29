f2e3374526a6b3982bffe996943aba0b341f245b
Yohann_Vioujard <y.vioujard@gmail.com>
2014-07-29 12:35:39 +0200

    Gruntfile revision.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,17 @@
 module.exports = function(grunt) {
-	/*
-	   Notes: regarder du coté de "jshint" comme module � ajouter.
+	/**
+	   Les imports des taches que peut gerer Grunt.
+	   Les modules correspondant ont étaient préalablement téléchargé via
+	   "npm" et enregistré dans le "package.json".
+	   Exemple de commandes de telechargement: "npm install grunt-contrib-sass --save-dev". ("--save-dev": enregistre dans les dépendances)
 	*/
+	grunt.loadNpmTasks('grunt-contrib-sass'); // /!\ Pour que "sass" soit opérationnel, il faut installer "Ruby" et "sass" avec "gem".
+	grunt.loadNpmTasks('grunt-jsdoc'); // Allows to generate html documentation. It needs Java, on windows Java must be append in the PATH
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync'); // Allows to synchronize the browsers during the development 
 
-	/*
-	   Les imports des taches que peut g�rer Grunt.
-	   Les modules correspondant ont �taient pr�alablement t�l�charg� via
-	   "npm" et enregistr� dans le "package.json".
-	   Exemple de commandes de telechargement: "npm install grunt-contrib-sass --save-dev".
-   */
-	grunt.loadNpmTasks('grunt-contrib-sass') // /!\ Pour que "sass" soit op�rationnel, il faut installer "Ruby" et "sass" avec "gem".
-	grunt.loadNpmTasks('grunt-jsdoc') // Allows to generate html documentation. It needs Java, on windows Java must be append in the PATH
-	grunt.loadNpmTasks('grunt-contrib-concat')
-	grunt.loadNpmTasks('grunt-contrib-watch')
-
-	/*
-	    On met le tableau des fichiers � concat�ner dans une variable.
-	*/
+	/** On met la liste des fichiers à concaténer dans une variable. */
 	var prefix = "dev/js/";
 	var jsSrc = [
 		prefix + 'Util.js',
@@ -30,9 +25,19 @@ module.exports = function(grunt) {
 		],
 		jsDist = 'dist/js/built.js';
 
-	// Configuration de Grunt
+	/** Configuration de Grunt */
 	grunt.initConfig({
-		sass: { // Compile les .scss
+		watch: { /** Permet de surveiller les fichiers, au moindre changement il lance la t�che attach�e au fichier. */
+			scripts: {
+				files: '**/*.js',
+				tasks: ['scripts:dev']
+			},
+			styles: {
+				files: '**/*.scss',
+				tasks: ['styles:dev']
+			}
+		},
+		sass: { /** Compile les .scss */
 			dist: {
 				options: {
 					style: 'expanded'
@@ -58,16 +63,16 @@ module.exports = function(grunt) {
 				}]
 			}
 		},
-		concat: { // Concat�ne les scripts en un seul.
+		concat: { /** Concatène les scripts en un seul. */
 			options: {
 				separator: ';'
 			},
-			compile: { // On renomme vu qu'on a pas de mode dev/dist. Dist �tant une autre t�che : uglify
+			compile: { /** On renomme vu qu'on a pas de mode dev/dist. Dist �tant une autre tache : uglify */
 				src: jsSrc,
 				dest: jsDist
 			}
 		},
-		uglify: { // Minifie les scripts
+		uglify: { /** Minifie les scripts */
 			options: {
 				separator: ';'
 			},
@@ -76,7 +81,7 @@ module.exports = function(grunt) {
 				dest: jsDist
 			}
 		},
-		jsdoc: { // G�n�re la documentation
+		jsdoc: { /** Génère la documentation */
 			generate: {
 				src: jsSrc,
 				options: {
@@ -84,24 +89,27 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		watch: { // Permet de surveiller les fichiers, au moindre changement il lance la t�che attach�e au fichier.
-			scripts: {
-				files: '**/*.js',
-				tasks: ['scripts:dev']
-			},
-			styles: {
-				files: '**/*.scss',
-				tasks: ['styles:dev']
-			}
-		}
+		browserSync: {
+            dev: {
+                bsFiles: {
+                    src : 'dist/css/*.css'
+                },
+                options: {
+					server: {
+						baseDir: "./"
+					},
+                    watchTask: true /** VERY important */
+                }
+            }
+        }
 	})
 
-	grunt.registerTask('default', ['dev', 'watch']) // C'est la tache lanc�e par d�faut en tapant juste "grunt" en ligne de commande.
-	grunt.registerTask('dev', ['styles:dev', 'scripts:dev']) // C'est la t�che lancer en tapant "grunt dev" en ligne de commande.
+	grunt.registerTask('default', ['browserSync', 'dev', 'watch']) // C'est la tache lancée par défaut en tapant juste "grunt" en ligne de commande.
+	grunt.registerTask('dev', ['styles:dev', 'scripts:dev']) // C'est la tache lancer en tapant "grunt dev" en ligne de commande.
 	grunt.registerTask('dist', ['styles:dist', 'scripts:dist']) // C'est la t�che lancer en tapant "grunt dist" en ligne de commande.
 	grunt.registerTask('doc', ['jsdoc:generate'])
 
-	// Des commandes et t�ches plus sp�cifiques
+	/** Des commandes et tâches plus spécifiques */
 	grunt.registerTask('scripts:dev', ['concat:compile'])
 	grunt.registerTask('scripts:dist', ['uglify:compile'])
 
